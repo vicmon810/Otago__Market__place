e5c8d700fd093e0870c2127dc128ec471f5bc87f
maokr879 <maokr879@student.otago.ac.nz>
2023-04-08 23:44:10 +1200

    error caues by faile connect, fix it

--- Project/server/db/conn.js
+++ Project/server/db/conn.js
@@ -1,11 +1,8 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
-
 const client = new MongoClient(Db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
 });
 
 var _db;
@@ -15,7 +12,7 @@ module.exports = {
     client.connect(function (err, db) {
       // Verify we got a good "db" object
       if (db) {
-        _db = db.db("employees");
+        _db = db.db("test");
         console.log("Successfully connected to MongoDB.");
       }
       return callback(err);
--- Project/server/server.js
+++ Project/server/server.js
@@ -1,14 +1,10 @@
-//middleware
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-<<<<<<< HEAD
-const port = process.env.PORT || 4000;
 
-=======
+require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 8000;
->>>>>>> d931330072fc3c90ed7cee392687d6acf491b2e6
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
@@ -19,6 +15,17 @@ const dbo = require("./db/conn");
 //router
 const itemRoute = require("./routes/item_routes.js");
 app.use("/api/item_routes", itemRoute);
+
+// mongoose.connect(port).then(() => {
+//   //LISTEN for requests
+//   app
+//     .listen(port, () => {
+//       console.log("Server is running on port : ${port}");
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// });
 
 app.listen(port, () => {
   // perform a database connection when server starts
