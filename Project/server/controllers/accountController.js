1060208c5878772e5c886bb34ab5a400b88d0d34
will <frawi873@student.otago.ac.nz>
2023-04-20 11:36:05 +1200

    added error handling for accountcontroller

--- Project/server/controllers/accountController.js
+++ Project/server/controllers/accountController.js
@@ -8,43 +8,86 @@ const ObjectId = require("mongodb").ObjectId;
 
 //get a single account info
 const getUser = async (req, res) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("user").findOne(myquery, function (err, result) {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
+  try{
+    let db_connect = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("user").findOne(myquery, function (err, result) {
+      if (err) throw err;
+      res.status(200).json(result);
+    });
+  }catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 //get admin info
 const getAdmin = async (req, res) => {
-  let db_connect = dbo.getDb("test");
-  db_connect
-    .collection("admin")
-    .find({})
-    .toArray(function (err, result) {
-      if (err) throw err;
-      res.status(200).json(result);
-    });
+  try{
+    let db_connect = dbo.getDb("test");
+    db_connect
+      .collection("admin")
+      .find({})
+      .toArray(function (err, result) {
+        if (err) throw err;
+        res.status(200).json(result);
+      });
+  }catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 //delete user account, should only be called by an admin account
 const deleteUser = async (req, res) => {
-  let db_connection = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connection.collection("user").deleteOne(myquery, function (err, result) {
-    if (err) throw err;
-    console.log("One account deleted successfully");
-    res.status(204).json(result);
-  });
+  try{
+    let db_connection = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    db_connection.collection("user").deleteOne(myquery, function (err, result) {
+      if (err) throw err;
+      console.log("One account deleted successfully");
+      res.status(204).json(result);
+    });
+  }catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 //UPDATE an existing account
 const updateUser = async (req, res) => {
-  let db_connection = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let userUpdate = {
-    $set: {
+  try{
+    let db_connection = dbo.getDb();
+    let myquery = { _id: ObjectId(req.params.id) };
+    let userUpdate = {
+      $set: {
+        User_id: req.body.User_id,
+        User_name: req.body.User_name,
+        password: req.body.password,
+        Email: req.body.email,
+        Department: req.body.Department,
+        Contact_Number: req.body.Contact_Number,
+        Profile_Picture: req.body.Profile_Picture,
+      },
+    };
+    db_connection
+    .collection("user")
+    .updateOne(myquery, userUpdate, function (err, result) {
+      if (err) throw err;
+      console.log("one user info updated");
+      res.status(200).json(result);
+    });
+  }catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+//create a new account
+const createAccount = async (req, res) => {
+  try {
+    let db_connect = dbo.getDb();
+    let account = {
       User_id: req.body.User_id,
       User_name: req.body.User_name,
       password: req.body.password,
@@ -52,34 +95,33 @@ const updateUser = async (req, res) => {
       Department: req.body.Department,
       Contact_Number: req.body.Contact_Number,
       Profile_Picture: req.body.Profile_Picture,
-    },
-  };
-  db_connection
-    .collection("user")
-    .updateOne(myquery, userUpdate, function (err, result) {
+    };
+    console.log(account);
+    db_connect.collection("user").insertOne(account, function (err, result) {
       if (err) throw err;
-      console.log("one user info updated");
-      res.status(200).json(result);
+      res.json(result);
     });
+  }catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
-//create a new account
-const createAccount = async (req, res) => {
-  let db_connect = dbo.getDb();
-  let account = {
-    User_id: req.body.User_id,
-    User_name: req.body.User_name,
-    password: req.body.password,
-    Email: req.body.email,
-    Department: req.body.Department,
-    Contact_Number: req.body.Contact_Number,
-    Profile_Picture: req.body.Profile_Picture,
-  };
-  console.log(account);
-  db_connect.collection("user").insertOne(account, function (err, result) {
-    if (err) throw err;
-    res.json(result);
-  });
+const verifyLogin = async (req, res) => {
+  try {
+    const db_connect = dbo.getDb();
+    const collection = db_connect.collection("user");
+    const result = await collection.findOne({User_Name: req.body.name});
+    // check if the password matches
+    if (result && result.password == req.body.password) {
+      res.status(200).json(result);
+    } else {
+      res.status(401).json({ message: "Invalid username or password" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 module.exports = {
