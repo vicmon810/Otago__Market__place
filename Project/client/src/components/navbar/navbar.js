71c81f504cb974b5df237687f9655f7b1fc3240f
Olivia <olivia2193249@gmail.com>
2023-05-02 12:03:45 +1200

    use conditional navbar depending if user logged in

--- Project/client/src/components/navbar/navbar.js
+++ Project/client/src/components/navbar/navbar.js
@@ -6,30 +6,59 @@ const Navbar = props => {
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
   const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <a className="navbar-brand text-info font-weight-bolder" href="/">
-        <span className="">otagoMarketplace</span>
-      </a>
-      <button className ="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
-        <span className ="navbar-toggler-icon"></span>
-      </button>
-      <div className ={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarToggleExternalContent">
-        <form className ="d-flex">
-          <input className="form-control me-2" type="search" placeholder="search otagoMarketplace" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit"
-          onClick={() => {
-            //TODO: add search functionality;
-          }}>Go</button>
-        </form>
-        <a className="nav-link text-info" href="/create"> Add Listing </a>
-        <a className="nav-link text-info" href="/login"> Login </a>
-        <a className="nav-link text-info" href="/register"> Register </a>
-        <a className="nav-link text-info" href="/profile"> Profile </a>
-      </div>
-    </nav>
-  );
-}
+  const [authenticated, setauthenticated] = useState(localStorage.getItem("authenticated"));
+  console.log('navbar authenticated:', authenticated);
+
+  if (authenticated==="true"){
+    console.log('LOGGED-IN NAVBAR');
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <a className="navbar-brand text-info font-weight-bolder" href="/">
+          <span className="">otagoMarketplace</span>
+        </a>
+        <button className ="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+          <span className ="navbar-toggler-icon"></span>
+        </button>
+        <div className ={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarToggleExternalContent">
+          <form className ="d-flex">
+            <input className="form-control me-2" type="search" placeholder="search otagoMarketplace" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit"
+            onClick={() => {
+              //TODO: add search functionality;
+            }}>Go</button>
+          </form>
+          <a className="nav-link text-info" href="/create"> Add Listing </a>
+          <a className="nav-link text-info" href="/profile"> Profile </a>
+        </div>
+      </nav>
+    );}
+    else {
+      console.log('FULL NAVBAR');
+      return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+          <a className="navbar-brand text-info font-weight-bolder" href="/">
+            <span className="">otagoMarketplace</span>
+          </a>
+          <button className ="custom-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded={!isNavCollapsed ? true : false} aria-label="Toggle navigation" onClick={handleNavCollapse}>
+            <span className ="navbar-toggler-icon"></span>
+          </button>
+          <div className ={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarToggleExternalContent">
+            <form className ="d-flex">
+              <input className="form-control me-2" type="search" placeholder="search otagoMarketplace" aria-label="Search"/>
+              <button className="btn btn-outline-success" type="submit"
+              onClick={() => {
+                //TODO: add search functionality;
+              }}>Go</button>
+            </form>
+            <a className="nav-link text-info" href="/create"> Add Listing </a>
+            <a className="nav-link text-info" href="/login"> Login </a>
+          <a className="nav-link text-info" href="/register"> Register </a>
+            <a className="nav-link text-info" href="/profile"> Profile </a>
+          </div>
+        </nav>
+      );
+    }
+  }
 
 export default Navbar;
 
