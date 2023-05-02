d5b83c99c3f8135a80a9a3b6bb9899125fc32027
Olivia <olivia2193249@gmail.com>
2023-05-02 12:15:39 +1200

    add logout button and redirect if logout

--- Project/client/src/components/profile.js
+++ Project/client/src/components/profile.js
@@ -1,12 +1,31 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router";
+
+function UseLogout(){
+  console.log('UseLogout()');
+  localStorage.setItem("authenticated",false);
+  localStorage.setItem("currUser",null);
+}
 
 export default function GetProfile() {
   const [profile, setProfile] = useState([]);
   const curruser = localStorage.getItem("currUser");
   const curruser_parsed=JSON.parse(curruser);
+  const navigate = useNavigate();
+
+  const [authenticated, setauthenticated] = useState(localStorage.getItem("authenticated"));
+
+  useEffect(() => {
+    async function checkLogin(){
+      if (authenticated==="false"){
+        navigate('/');
+      }}
+    checkLogin();
+});
 
   useEffect(() => {
     async function getUser() {
+      if (curruser_parsed){
       const response = await fetch(`http://localhost:8000/api/account_routes/account/${curruser_parsed._id}`, {method: "GET"});
       if (!response.ok) {
         const message = `An error occured: ${response.statusText}`;
@@ -15,7 +34,7 @@ export default function GetProfile() {
       }
       const user = await response.json();
       setProfile(user);
-    }
+    }}
     getUser();
     return;
   });
@@ -28,6 +47,10 @@ export default function GetProfile() {
       <p>Phone Number: {profile.number}</p>
       <p>Department: {profile.department}</p>
       <p>Account activated on: {profile.activationDate}</p>
+
+      <form onSubmit={UseLogout}>
+      <button type="submit"> Logout </button>
+      </form>
     </div>
   );
 }
