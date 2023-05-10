8c62d6558e42f8c753ab845ade8fdba3fe95b210
Will Frame <frawi873@student.otago.ac.nz>
2023-05-11 01:11:50 +1200

    fixed email

--- Project/client/src/components/sendMessage/sendMessage.js
+++ Project/client/src/components/sendMessage/sendMessage.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useLocation } from "react-router";
 
 export default function Create() {
+  let itemTitle = "";
+  let userTitle = "";
+
   const [form, setForm] = useState({
     username: "",
     item: "",
     message: "",
   });
   const navigate = useNavigate();
+  const location = useLocation();
+  const query = new URLSearchParams(location.search);
+  let itemId = query.get("item");
+  console.log(itemId)
+  
+  // The email works however for prefilling values I can't figure out how to get the following to fill out the
+  // form, trying to print them prints nothing. I also am not sure how to get this to only run once, it seems to
+  // be continuously running whenever the user modifies any value.
+  async function getItemInfo(itemId){
+    const itemData = await fetch("http://localhost:8000/api/item_routes/item/" + itemId);
+    itemTitle = itemData.body.title;
+    const userData = await fetch("http://localhost:8000/api/account_routes/account/" + itemData.body.userAccount);
+    userTitle = userData.body.name;
+  }
+
+  getItemInfo(itemId);
+
+  console.log(itemTitle);
+  console.log(userTitle);
 
   // These methods will update the state properties.
   function updateForm(value) {
@@ -34,8 +56,8 @@ export default function Create() {
     });
 
     setForm({
-      username: "",
-      item: "",
+      username: userTitle,
+      item: itemTitle,
       message: "",
     });
     window.alert("Message sent!");
